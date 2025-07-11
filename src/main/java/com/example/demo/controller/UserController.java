f46883917cb9fc5dafd77bb31499c45ef4b990d5
Khant Zin <khantzinwin123@gmail.com>
2025-07-11 21:48:33 +0700

    first commit

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import com.example.demo.record.UserRecord;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @GetMapping("/getUser")
+    public String getUser() {
+        return new UserRecord("Kaolin", "kaolin@gmail.com", "123 Main St")
+                .toString();
+    }
+}
---
+++ src/main/java/com/example/demo/record/UserRecord.java
@@ -0,0 +1,4 @@
+package com.example.demo.record;
+
+
+public record UserRecord(String name, String email, String address) {}
